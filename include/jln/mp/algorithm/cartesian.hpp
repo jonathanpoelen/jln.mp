6a02e0afda96ae8ab6a33f44046c5f97a23ccee2
Jonathan Poelen <jonathan.poelen@gmail.com>
2019-12-06 01:23:23 +0100

    indentation

--- include/jln/mp/algorithm/cartesian.hpp
+++ include/jln/mp/algorithm/cartesian.hpp
@@ -34,82 +34,82 @@ namespace jln::mp
 
 namespace jln::mp::detail
 {
-    template<class, class>
-    struct _product;
+  template<class, class>
+  struct _product;
 
-    template<class... xs, class... ys>
-    struct _product<list<xs...>, list<ys...>>
-    {
-      using type = list<list<xs..., ys>...>;
-    };
+  template<class... xs, class... ys>
+  struct _product<list<xs...>, list<ys...>>
+  {
+    using type = list<list<xs..., ys>...>;
+  };
 
-    template<class... ys>
-    struct _product<list<>, list<ys...>>
-    {
-      using type = list<>;
-    };
+  template<class... ys>
+  struct _product<list<>, list<ys...>>
+  {
+    using type = list<>;
+  };
 
-    template<class>
-    struct _product1;
+  template<class>
+  struct _product1;
 
-    template<class... xs>
-    struct _product1<list<xs...>>
-    {
-      using type = list<list<xs>...>;
-    };
+  template<class... xs>
+  struct _product1<list<xs...>>
+  {
+    using type = list<list<xs>...>;
+  };
 
-    template<class C>
-    struct _cartesian<C, 0>
-    {
-      template<class...>
-      using f = typename C::template f<>;
-    };
+  template<class C>
+  struct _cartesian<C, 0>
+  {
+    template<class...>
+    using f = typename C::template f<>;
+  };
 
-    template<>
-    struct _cartesian<listify, 0>
-    {
-      template<class...>
-      using f = list<>;
-    };
+  template<>
+  struct _cartesian<listify, 0>
+  {
+    template<class...>
+    using f = list<>;
+  };
 
-    template<class C>
-    struct _cartesian<C, 1>
-    {
-      template<class seq>
-      using f = typename C::template f<
-        typename _product1<seq>::type>;
-    };
+  template<class C>
+  struct _cartesian<C, 1>
+  {
+    template<class seq>
+    using f = typename C::template f<
+      typename _product1<seq>::type>;
+  };
 
-    template<>
-    struct _cartesian<listify, 1>
-    {
-      template<class seq>
-      using f = typename _product1<seq>::type;
-    };
+  template<>
+  struct _cartesian<listify, 1>
+  {
+    template<class seq>
+    using f = typename _product1<seq>::type;
+  };
 
-    template<class, class>
-    struct _cartesian_impl;
+  template<class, class>
+  struct _cartesian_impl;
 
-    template<class... seqs, class seq>
-    struct _cartesian_impl<list<seqs...>, seq>
-    {
-      using type = typename join<>
-        ::f<typename _product<seqs, seq>::type...>;
-    };
+  template<class... seqs, class seq>
+  struct _cartesian_impl<list<seqs...>, seq>
+  {
+    using type = typename join<>
+      ::f<typename _product<seqs, seq>::type...>;
+  };
 
-    template<class C, unsigned n>
-    struct _cartesian
-    {
-      template<class seq, class... seqs>
-      using f = typename fold_left<cfl<detail::_cartesian_impl>, unpack<C>>
-        ::template f<typename _product1<seq>::type, seqs...>;
-    };
+  template<class C, unsigned n>
+  struct _cartesian
+  {
+    template<class seq, class... seqs>
+    using f = typename fold_left<cfl<detail::_cartesian_impl>, unpack<C>>
+      ::template f<typename _product1<seq>::type, seqs...>;
+  };
 
-    template<unsigned n>
-    struct _cartesian<listify, n>
-    {
-      template<class seq, class... seqs>
-      using f = typename fold_left<cfl<detail::_cartesian_impl>>
-        ::template f<typename _product1<seq>::type, seqs...>;
-    };
+  template<unsigned n>
+  struct _cartesian<listify, n>
+  {
+    template<class seq, class... seqs>
+    using f = typename fold_left<cfl<detail::_cartesian_impl>>
+      ::template f<typename _product1<seq>::type, seqs...>;
+  };
 }
