5eb796f99036edfce9395c2fcc668d1c26160a3b
Jonathan Poelen <jonathan.poelen@gmail.com>
2020-02-29 20:36:36 +0100

    use std::type_identity if c++20

--- include/jln/mp/detail/type_identity.hpp
+++ include/jln/mp/detail/type_identity.hpp
@@ -1,11 +1,19 @@
 #pragma once
 
+#if __cplusplus > 201703L
+#include <type_traits>
+
 namespace jln::mp::detail
 {
-  // TODO C++20
+  using std::type_identity;
+}
+#else
+namespace jln::mp::detail
+{
   template<class T>
   struct type_identity
   {
     using type = T;
   };
 }
+#endif
