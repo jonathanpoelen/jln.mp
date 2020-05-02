fa9388330370300a5f9cea363b8f4b15453f6994
Jonathan Poelen <jonathan.poelen@gmail.com>
2020-05-02 04:00:46 +0200

    [meson] use tools/optimize_ninja.sh

--- tools/optimize_ninja.sh
+++ tools/optimize_ninja.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 if [ -d "$1" ] ;then
   f=$1/build.ninja
+elif [ -d "$MESON_BUILD_ROOT" ]; then
+  f=$MESON_BUILD_ROOT/build.ninja
 fi
 
 sed -i -E '
-s/^( command = .*) -o \$out -c \$in/\1 -fsyntax-only $in \&\& :>$out/
-s/^ command = .*\$LINK_ARGS/ command = :>$out/
+s/^( command = .*) -o \$out -c \$in/\1 -fsyntax-only $in \&\& :>$out/;t
+s/^(build [^:]+): cpp_LINKER/\1: phony/;Ta;n;d;:a
 ' "${f:-build.ninja}"
