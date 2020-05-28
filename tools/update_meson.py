c3068d9fe7dd48feeed0fa8aafb2f916725dd8a5
Jonathan Poelen <jonathan.poelen@gmail.com>
2020-05-28 03:19:51 +0200

    [tool] update_meson.py: sort files and rename targets to reduce update differences

--- tools/update_meson.py
+++ tools/update_meson.py
@@ -6,6 +6,11 @@ all_targets = set()
 cpp_test_paths = []
 output_lines = []
 
+def ordered_listdir(path):
+  l = os.listdir(path)
+  l.sort()
+  return l
+
 def new_target(name):
   while name in all_targets:
     name += '_'
@@ -21,25 +26,22 @@ def new_alias(name, targets):
   deps = ", ".join(targets)
   output_lines.append(f"alias_target('{name}', {deps})")
 
-def make_targets(path, prefix, target_prefix='t_'):
+def make_targets(path, prefix, target_prefix='t'):
   l = []
-  i = 0
-  idir = 0
-  for f in os.listdir(path):
+  for f in ordered_listdir(path):
     newpath = os.path.join(path, f)
     if os.path.isfile(newpath):
       if f.endswith('.cpp'):
+        basename = f[:-4]
         cpp_test_paths.append(f'{path}/{f}')
-        name = new_target(f[:-4])
-        l.append(new_executable(name, newpath, target_prefix, i))
-        i += 1
+        name = new_target(basename)
+        l.append(new_executable(name, newpath, target_prefix, f'_{basename}'))
     else:
       dirtarget = f'{prefix}.{f}'
-      targets = make_targets(newpath, dirtarget, f'{target_prefix}{idir}_')
+      targets = make_targets(newpath, dirtarget, f'{target_prefix}_{f}')
       name = new_target(dirtarget)
       new_alias(name, targets)
       l += targets
-      idir += 1
   return l
 
 new_alias('mp', make_targets('test/src', 'mp'))
@@ -57,12 +59,11 @@ with open('meson.build', 'w') as f:
   f.write('\n'.join(output_lines))
   f.write('\n')
   sources = "',\n  '".join(os.path.join('test/autogen', filename)
-                        for filename in os.listdir('test/autogen'))
+                        for filename in ordered_listdir('test/autogen'))
   f.write(f"executable('check_inc', [\n  '{sources}'\n], dependencies: test_dep)\n")
   f.write(stop_str)
   f.write(content[stop + len(stop_str):])
 
-cpp_test_paths.sort()
 with open('test/mp.cpp', 'w') as f:
   for path in cpp_test_paths:
     f.write(f'#include "{path[5:]}"\n')
