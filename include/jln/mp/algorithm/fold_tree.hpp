0bf8cd87c7d6445967fdde33a8ff3af5c4e0452f
Jonathan Poelen <jonathan.poelen@gmail.com>
2021-04-19 02:44:24 +0200

    #include "..." -> <...>

--- include/jln/mp/algorithm/fold_tree.hpp
+++ include/jln/mp/algorithm/fold_tree.hpp
@@ -66,8 +66,8 @@ namespace jln::mp
 }
 
 
-#include "jln/mp/list/take_front.hpp"
-#include "jln/mp/list/drop_front.hpp"
+#include <jln/mp/list/take_front.hpp>
+#include <jln/mp/list/drop_front.hpp>
 
 /// \cond
 namespace jln::mp::detail
