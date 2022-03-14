eae732b1c37cefdc5fd14ca70dede8af8849b522
Jonathan Poelen <jonathan.poelen@gmail.com>
2022-03-14 18:15:08 +0100

    fix smp::partition with mp_debug=true

--- include/jln/mp/smp/algorithm/partition.hpp
+++ include/jln/mp/smp/algorithm/partition.hpp
@@ -1,22 +1,45 @@
 #pragma once
 
-#include <jln/mp/smp/algorithm/transform.hpp>
-#include <jln/mp/smp/list/join.hpp>
+#include <jln/mp/smp/assume.hpp>
+#include <jln/mp/smp/contract.hpp>
+#include <jln/mp/smp/list/listify.hpp>
 #include <jln/mp/algorithm/partition.hpp>
+
+/// \cond
+namespace jln::mp::detail
+{
+  template<class Pred, class F, class C>
+  struct smp_partition_with;
+}
+/// \endcond
 
 namespace jln::mp::smp
 {
   template<class Pred, class F = listify, class C = listify>
-  using partition_with = mp::partition_with<
-      try_assume_unary<Pred>, subcontract<F>, subcontract<C>>;
+  using partition_with = try_contract<detail::smp_partition_with<
+      assume_unary<Pred>, subcontract<F>, subcontract<C>>>;
 
   template<class Pred, class C = listify>
   using partition = partition_with<Pred, listify, C>;
 }
 
+
+#include <jln/mp/functional/monadic.hpp>
+
 /// \cond
 namespace jln::mp::detail
 {
+  template<class Pred, class F, class C>
+  struct smp_partition_with
+  {
+    template<class... xs>
+    using f = typename mp::transform<
+      Pred,
+      mp::try_or<partition_caller, violation>
+    >::template f<xs...>
+     ::template f<mp::monadic_xs<C>, F, xs...>;
+  };
+
   template<template<class> class sfinae, class Pred, class F, class C>
   struct _sfinae<sfinae, partition_with<Pred, F, C>>
   {
