d4677d3a4a56ccec99ffed7c87019e477b707bba
Jonathan Poelen <jonathan.poelen@gmail.com>
2022-04-23 16:36:16 +0200

    refactor range

--- include/jln/mp/list/range.hpp
+++ include/jln/mp/list/range.hpp
@@ -9,11 +9,10 @@ namespace jln::mp
   /// \cond
   namespace detail
   {
-    template<int_ beg, int_ end, class C, bool _
-      = !(beg == -1
-      || ((beg >= 0 ? end >= 0 : end < 0) && beg >= end)),
-      bool = _ && (beg >= 0),
-      bool = _ && (end >= 0)>
+    template<int_ beg, int_ end,
+      bool = beg < 0,
+      bool = end < 0 || beg == -1,
+      bool = beg >= end || beg == -1>
     struct _range;
   }
   /// \endcond
@@ -21,11 +20,11 @@ namespace jln::mp
   /// \ingroup list
 
   /// Returns a contiguous subsequence of a \sequence.
-  /// A negative value represents an index starting from the end.
+  /// A negative value represents an index starting from the end + 1.
   /// if finally, \c end <= \c beg, then an empty \list is returned.
   /// \treturn \sequence
   template<int_ beg, int_ end, class C = listify>
-  struct range_c : detail::_range<beg, end, C>::type
+  struct range_c : detail::_range<beg, end>::template impl<C>
   {
 #ifdef JLN_MP_DOXYGENATING
     template<class... xs>
@@ -46,117 +45,128 @@ namespace jln::mp
   }
 }
 
-#include <jln/mp/utility/always.hpp>
 #include <jln/mp/list/clear.hpp>
-#include <jln/mp/list/take_front.hpp>
 #include <jln/mp/list/drop_front.hpp>
+#include <jln/mp/algorithm/rotate.hpp>
+#include <jln/mp/detail/compiler.hpp>
 
 /// \cond
 namespace jln::mp::detail
 {
-  template<int_ beg, int_ end, class C>
-  struct _range<beg, end, C, false, false, false>
+  template<bool, bool>
+  struct range_impl
   {
-    using type = clear<C>;
+    template<class C, int_ beg, int_ end, int_ n>
+    using f = clear<C>;
   };
 
+  template<>
+  struct range_impl<true, true>
+  {
+    template<class C, int_ beg, int_ end, int_ n>
+    using f = rotate_c<end, drop_front_c<n - end + beg, C>>;
+  };
+
+  template<>
+  struct range_impl<true, false>
+  {
+    template<class C, int_ beg, int_ end, int_ n>
+    using f = drop_front_c<beg, C>;
+  };
+
+  template<class C, int_ beg, int_ end, int_ n>
+  using range_select = typename range_impl<beg < n, end < n>
+    ::template f<C, beg, end, n>;
+
+  template<>
+  struct _range<0, -1, false, true, false>
+  {
+    template<class C>
+    using impl = C;
+  };
+
+  // end >= 0 && beg >= end
   template<int_ beg, int_ end>
-  struct _range<beg, end, listify, false, false, false>
+  struct _range<beg, end, false, false, true>
   {
-    using type = always<list<>>;
+    template<class C>
+    using impl = clear<C>;
   };
 
-  template<class C>
-  struct _range<0, -1, C, true, true, false>
+  // end < 0 && beg < 0 && beg >= end
+  // or
+  // beg = -1
+  template<int_ beg, int_ end>
+  struct _range<beg, end, true, true, true>
   {
-    using type = C;
+    template<class C>
+    using impl = clear<C>;
   };
 
-  template<int_ beg, int_ end, class C, bool b1, bool b2>
-  struct _range<beg, end, C, true, b1, b2>
+  // beg < 0 and end < 0
+  template<int_ beg, int_ end, bool b3>
+  struct _range<beg, end, true, true, b3>
   {
+    template<class C>
     struct impl
     {
       template<class... xs>
-      using f = typename range_c<
-        (beg >= 0
-          ? beg
-          : (sizeof...(xs) <= -beg
-            ? 0
-            : int_(sizeof...(xs)) + beg)),
-        (end >= 0
-          ? end
-          : (sizeof...(xs) <= -end
-            ? 0
-            : int_(sizeof...(xs)) + (end+1))),
-        C
+      using f = typename range_select<C,
+        sizeof...(xs) <= -beg ? 0 : int_(sizeof...(xs)) + beg,
+        sizeof...(xs) <= -end ? 0 : int_(sizeof...(xs)) + end + 1,
+        sizeof...(xs)
       >::template f<xs...>;
     };
-
-    using type = impl;
   };
 
-  template<int_ beg, int_ end, int_ n, class C,
-    bool = (beg < n), bool = (end < n)>
-  struct _range_impl // true, true
+  // beg >= 0 and end < 0
+  template<int_ beg, int_ end, bool b3>
+  struct _range<beg, end, false, true, b3>
   {
-    using type = rotate_c<end, drop_front_c<n - end + beg, C>>;
-  };
-
-  template<int_ end, int_ n, class C>
-  struct _range_impl<0, end, n, C, true, true>
-  {
-    using type = take_front_c<end, C>;
-  };
-
-  template<int_ beg, int_ end, int_ n, class C>
-  struct _range_impl<beg, end, n, C, true, false>
-  {
-    using type = drop_front_c<beg, C>;
-  };
-
-  template<int_ end, int_ n, class C>
-  struct _range_impl<0, end, n, C, true, false>
-  {
-    using type = JLN_MP_TRACE_F(C);
-  };
-
-  template<int_ beg, int_ end, int_ n, class C>
-  struct _range_impl<beg, end, n, C, false, false>
-  {
-    using type = clear<C>;
-  };
-
-  template<int_ beg, int_ end, int_ n>
-  struct _range_impl<beg, end, n, listify, false, false>
-  {
-    using type = always<list<>>;
-  };
-
-  template<int_ beg, int_ end, class C>
-  struct _range<beg, end, C, true, true, true>
-  {
+    template<class C>
     struct impl
     {
       template<class... xs>
-      using f = typename _range_impl<beg, end, sizeof...(xs), C>
-        ::type::template f<xs...>;
+      using f = typename range_select<C,
+        beg,
+        sizeof...(xs) <= -end ? 0 : int_(sizeof...(xs)) + end + 1,
+        sizeof...(xs)
+      >::template f<xs...>;
     };
-
-    using type = impl;
   };
 
-  template<int_ beg, class C>
-  struct _range<beg, -1, C, true, true, false>
+  // beg < 0 and end >= 0
+  template<int_ beg, int_ end, bool b3>
+  struct _range<beg, end, true, false, b3>
   {
+    template<class C>
     struct impl
     {
       template<class... xs>
-      using f = typename _range_impl<beg, sizeof...(xs), sizeof...(xs), C>
-        ::type::template f<xs...>;
+      using f = typename range_select<C,
+        sizeof...(xs) <= -beg ? 0 : int_(sizeof...(xs)) + beg,
+        end,
+        sizeof...(xs)
+      >::template f<xs...>;
     };
+  };
 
-    using type = impl;
+  // beg >= 0 and end >= 0
+  template<int_ beg, int_ end, bool b3>
+  struct _range<beg, end, false, false, b3>
+  {
+    template<class C>
+    struct impl
+    {
+      template<class... xs>
+#if JLN_MP_GCC
+      using f = typename range_select<C, beg, end, sizeof...(xs)>
+#else
+      using f = typename range_impl<beg < sizeof...(xs), end < sizeof...(xs)>
+        ::template f<C, beg, end, sizeof...(xs)>
+#endif
+        ::template f<xs...>;
+    };
   };
 }
 /// \endcond
