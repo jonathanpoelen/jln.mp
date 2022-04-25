86a397d0f44bc9d127e0e770698464ad2969be7f
Jonathan Poelen <jonathan.poelen@gmail.com>
2022-04-25 19:14:28 +0200

    optimize swap_index

--- include/jln/mp/list/swap_index.hpp
+++ include/jln/mp/list/swap_index.hpp
@@ -9,7 +9,7 @@ namespace jln::mp
   /// \cond
   namespace detail
   {
-    template<unsigned i, unsigned j, bool = (i < j), bool = (i == j)>
+    template<bool, bool>
     struct _select_swap_index;
   }
   /// \endcond
@@ -21,13 +21,12 @@ namespace jln::mp
   /// \pre `0 < J < sizeof...(xs)`
   /// \treturn \sequence
   /// \note `swap_index<I, J>` == `swap_index<J, I>`
-  template<class I, class J, class C = listify>
-  using swap_index = typename detail::_select_swap_index<
-    unsigned{I::value}, unsigned{J::value}>::template f<C>;
-
   template<unsigned i, unsigned j, class C = listify>
-  using swap_index_c = typename detail::_select_swap_index<
-    i, j>::template f<C>;
+  using swap_index_c = typename detail::_select_swap_index<i < j, i == j>
+    ::template f<i, j, C>;
+
+  template<class I, class J, class C = listify>
+  using swap_index = swap_index_c<I::value, J::value, C>;
 
   namespace emp
   {
@@ -41,38 +40,66 @@ namespace jln::mp
 
 
 #include <jln/mp/algorithm/rotate.hpp>
-#include <jln/mp/list/join.hpp>
-#include <jln/mp/list/drop_front.hpp>
-#include <jln/mp/list/at.hpp>
-#include <jln/mp/functional/call.hpp>
 
 /// \cond
 namespace jln::mp::detail
 {
+  //                     |        i     j
+  //                     |        <--n-->
+  //                     |    aaa X bbb Y ccc
+  //                     |
+  // rotate i            |    X bbb Y ccc aaa
+  // get X ; prepend C,i |  C i bbb Y ccc aaa
+  // rotate n+1          |    Y ccc aaa C i bbb
+  // get Y ; append X    |      ccc aaa C i bbb X
+  // rotate -n-1         |    C i bbb X ccc aaa
+  // rm C,i ; append Y   |        bbb X ccc aaa Y
+  //                     |
+  // rotate -i           |    aaa Y bbb X ccc
+
+  template<class y>
+  struct swap_elem_push_Y
+  {
+    template<class C, class i, class... xs>
+    using f = typename rotate_impl<sizeof...(xs) - i::value>
+      ::template f<sizeof...(xs) - i::value, C, xs..., y>;
+  };
+
+  template<class x, unsigned n>
+  struct swap_elem_get_Y_push_X
+  {
+    template<class y, class... xs>
+    using f = typename rotate_impl<sizeof...(xs) - n - 1>
+      ::template f<sizeof...(xs) - n - 1, swap_elem_push_Y<y>, xs..., x>;
+  };
+
+  template<unsigned n, class i, class C>
+  struct swap_elem_get_X
+  {
+    template<class x, class... xs>
+    using f = typename rotate_impl<(sizeof...(xs) & 0) + n + 1>
+      ::template f<n + 1, swap_elem_get_Y_push_X<x, n>, C, i, xs...>;
+  };
+
   template<unsigned i, unsigned j, class C>
   struct swap_elem
   {
     template<class... xs>
-    using f = typename join<C>::template f<
-      typename rotate_c<i, drop_front_c<sizeof...(xs) - i>>::template f<xs...>,
-      list<typename at_c<j>::template f<xs...>>,
-      typename rotate_c<j, drop_front_c<sizeof...(xs) - j + i + 1>>::template f<xs...>,
-      list<typename at_c<i>::template f<xs...>>,
-      typename drop_front_c<j+1>::template f<xs...>
-    >;
+    using f = typename rotate_impl<(sizeof...(xs) & 0) + i>
+      ::template f<i, swap_elem_get_X<j-i, number<i>, C>, xs...>;
   };
 
-  template<unsigned i, unsigned j, bool, bool>
+  template<bool, bool>
   struct _select_swap_index
   {
-    template<class C>
+    template<unsigned i, unsigned j, class C>
     using f = swap_elem<i, j, C>;
   };
 
-  template<unsigned i, unsigned j>
-  struct _select_swap_index<i, j, false, false>
+  template<>
+  struct _select_swap_index<false, false>
   {
-    template<class C>
+    template<unsigned i, unsigned j, class C>
     using f = swap_elem<j, i, C>;
   };
 
@@ -90,10 +117,10 @@ namespace jln::mp::detail
     using f = list<xs...>;
   };
 
-  template<unsigned i, unsigned j>
-  struct _select_swap_index<i, j, false, true>
+  template<>
+  struct _select_swap_index<false, true>
   {
-    template<class C>
+    template<unsigned i, unsigned j, class C>
     using f = if_valid_index<i, C>;
   };
 }
