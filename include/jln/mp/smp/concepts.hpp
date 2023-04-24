c27336fafe1013783ddf9ff733db71da68bd04f3
Jonathan Poelen <jonathan.poelen@gmail.com>
2023-04-24 20:37:12 +0200

    simplify oconcepts::predicate / strong_predicate

--- include/jln/mp/smp/concepts.hpp
+++ include/jln/mp/smp/concepts.hpp
@@ -4,18 +4,16 @@
 #include <jln/mp/utility/always.hpp>
 #include <jln/mp/number/as_bool.hpp>
 #include <jln/mp/number/to_bool.hpp>
-#include <jln/mp/functional/tee.hpp>
 
 namespace jln::mp::smp::concepts
 {
   // TODO optimize Pred = is<>, same<>, lift<std::is_same>, equal, less, not_
-  // TODO optimize when FC = always
   // {unary, binary, mulary}_predicate
   template<class Pred, class TC = mp::identity, class FC = mp::always<false_>>
-  using predicate = mp::try_<mp::tee<mp::try_<Pred>, mp::try_<mp::to_bool<>>>, TC, FC>;
+  using predicate = mp::try_<Pred, mp::try_<mp::to_bool<>, TC, FC>, FC>;
 
   template<class Pred, class TC = mp::identity, class FC = mp::always<false_>>
-  using strong_predicate = mp::try_<mp::tee<mp::try_<Pred>, mp::try_<mp::as_bool<>>>, TC, FC>;
+  using strong_predicate = mp::try_<Pred, mp::try_<mp::as_bool<>, TC, FC>, FC>;
 
   template<class Pred, class FC>
   using predicate_or = predicate<Pred, mp::identity, FC>;
