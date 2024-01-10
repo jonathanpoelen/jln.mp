4f33370b7f2af84fa911489a7ad9d09313b26e0a
Jonathan Poelen <jonathan.poelen@gmail.com>
2024-01-10 13:56:32 +0100

    run optimize_ninja.sh with MacOS

--- tools/optimize_ninja.sh
+++ tools/optimize_ninja.sh
@@ -1,15 +1,19 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 # SPDX-FileCopyrightText: 2023 Jonathan Poelen <jonathan.poelen@gmail.com>
 # SPDX-License-Identifier: MIT
 
-if [ -d "$1" ] ;then
-  f=$1/build.ninja
-elif [ -d "$MESON_BUILD_ROOT" ]; then
-  f=$MESON_BUILD_ROOT/build.ninja
+if [[ -d "$1" ]]; then
+  f="$1"/build.ninja
+elif [[ -f "$1" ]]; then
+  f="$1"
+elif [[ -d "$MESON_BUILD_ROOT" ]]; then
+  f="$MESON_BUILD_ROOT"/build.ninja
+else
+  f=build.ninja
 fi
 
-sed -i -E '
-s/^( command = .*) -o \$out -c \$in/\1 -fsyntax-only $in \&\& :>$out/;t
-s/-g[0-9]? |-O[1-3] //g;t
-s/^(build [^:]+): cpp_LINKER/\1: phony/;Ta;n;d;:a
-' "${f:-build.ninja}"
+sed -i '
+s/^\( command = .*\) -o \$out -c \$in/\1 -fsyntax-only $in \&\& :>$out/;t
+s/-g[0-9]\? \|-O[1-3] //g;t
+s/^\(build [^:]\+\): cpp_LINKER/\1: phony/;Ta;n;d;:a
+' "$f"
