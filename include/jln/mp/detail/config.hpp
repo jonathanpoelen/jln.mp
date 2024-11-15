cb9570881aab297e4b5376e885dee7a3581c0e3d
Jonathan Poelen <jonathan.poelen@gmail.com>
2024-11-15 01:49:32 +0100

    fix the debug config for val
    With some compilers, the debug value is forced to 0,
    which disables the alternative `val` implementation for gcc and msvc.
    A new macro is introduced to preserve the original value.

--- include/jln/mp/detail/config.hpp
+++ include/jln/mp/detail/config.hpp
@@ -35,6 +35,12 @@
 # define JLN_MP_ENABLE_DEBUG 0
 #endif
 
+#if JLN_MP_ENABLE_DEBUG
+# define JLN_MP_ENABLE_DEBUG_ORIGINAL 1
+#else
+# define JLN_MP_ENABLE_DEBUG_ORIGINAL 0
+#endif
+
 // Useless with gcc and msvc since it displays all the transformations.
 #if JLN_MP_GCC || JLN_MP_MSVC || JLN_MP_MEMOIZED_ALIAS
 # undef JLN_MP_ENABLE_DEBUG
--- include/jln/mp/value/val.hpp
+++ include/jln/mp/value/val.hpp
@@ -5,8 +5,12 @@
 #include <jln/mp/detail/config.hpp>
 #include <jln/mp/number/number.hpp>  // JLN_MP_ENABLE_TPL_AUTO
 
-#if !JLN_MP_ENABLE_TPL_AUTO || (JLN_MP_ENABLE_DEBUG && !JLN_MP_CLANG_LIKE)
-#  include <type_traits>
+#ifndef JLN_MP_VAL_AS_ALIAS
+# define JLN_MP_VAL_AS_ALIAS (JLN_MP_ENABLE_DEBUG_ORIGINAL && !JLN_MP_CLANG_LIKE)
+#endif
+
+#if !JLN_MP_ENABLE_TPL_AUTO || JLN_MP_VAL_AS_ALIAS
+# include <type_traits>
 #endif
 
 namespace jln::mp
@@ -14,7 +18,10 @@ namespace jln::mp
   /// \ingroup value
 
 #if JLN_MP_ENABLE_TPL_AUTO
-# if !JLN_MP_ENABLE_DEBUG || JLN_MP_CLANG_LIKE
+# if !JLN_MP_VAL_AS_ALIAS
+  // note: integer types are not displayed with some compilers
+  // clang: val<0ll>
+  // gcc: val<0> with 0 as long long
   template<auto v>
   struct val
   {
@@ -53,3 +60,5 @@ namespace jln::mp
   using value_from = typed_value<std::remove_const_t<decltype(T::value)>, T::value>;
 #endif
 }
+
+#undef JLN_MP_VAL_AS_ALIAS
