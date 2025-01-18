699af1d00dc2dd6dbd3e1b260339efe0db90338a
Jonathan Poelen <jonathan.poelen@gmail.com>
2025-01-18 17:18:04 +0100

    optimize scan when F is a lift

--- include/jln/mp/algorithm/scan.hpp
+++ include/jln/mp/algorithm/scan.hpp
@@ -45,8 +45,22 @@ namespace jln::mp
 
 
 #include <jln/mp/list/join.hpp>
+#include <jln/mp/detail/compiler.hpp>
 
 /// \cond
+#if ! JLN_MP_OPTIMIZED_ALIAS && ! JLN_MP_ENABLE_DEBUG
+namespace jln::mp
+{
+  template<template<class...> class F, class C>
+  struct scan<lift<F>, C>
+  {
+    template<class... xs>
+    using f = typename detail::_scan<sizeof...(xs)>
+      ::template f<sizeof...(xs), C, F, xs...>;
+  };
+}
+#endif
+
 namespace jln::mp::detail
 {
   template<unsigned n>
@@ -364,8 +378,8 @@ namespace jln::mp::detail
       _16, _17, _18, _19, _20, _21, _22, _23, _24, _25, _26, _27, _28, _29, _30,
       _31, _32, _33, _34, _35, _36, _37, _38, _39, _40, _41, _42, _43, _44, _45,
       _46, _47, _48, _49, _50, _51, _52, _53, _54, _55, _56, _57, _58, _59, _60,
-      _61, _62,
-      _63>::template f<remaining - 63, _scan<remaining - 63>, C, F, xs...>;
+      _61, _62, _63
+    >::template f<remaining - 63, _scan<remaining - 63>, C, F, xs...>;
   };
 
   template<>
@@ -486,8 +500,8 @@ namespace jln::mp::detail
       _76, _77, _78, _79, _80, _81, _82, _83, _84, _85, _86, _87, _88, _89, _90,
       _91, _92, _93, _94, _95, _96, _97, _98, _99, _100, _101, _102, _103, _104,
       _105, _106, _107, _108, _109, _110, _111, _112, _113, _114, _115, _116,
-      _117, _118, _119, _120, _121, _122, _123, _124, _125, _126, _127>::
-      template f<remaining - 127, _scan<remaining - 127>, C, F, xs...>;
+      _117, _118, _119, _120, _121, _122, _123, _124, _125, _126, _127
+    >::template f<remaining - 127, _scan<remaining - 127>, C, F, xs...>;
   };
 }
 /// \endcond
