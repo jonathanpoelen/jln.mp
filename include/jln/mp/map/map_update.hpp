2fe5b1b2b3f9974c6ea4cfeb8e3961f01ec89d21
Jonathan Poelen <jonathan.poelen@gmail.com>
2025-02-08 02:08:48 +0100

    map_update: rename some parameters: Key -> key ; KV -> kv ; x -> kv

--- include/jln/mp/map/map_update.hpp
+++ include/jln/mp/map/map_update.hpp
@@ -21,12 +21,12 @@ namespace jln::mp
   using map_update = transform<map_find<Key, F, front<>>, C>;
 
   /// Update an element `L<k, v...>` with `L<k, F<k, v...>>`.
-  /// \pre `unpack<size<>>::f<x> >= 1`
+  /// \pre `unpack<size<>>::f<kv> >= 1`
   template<class F>
   struct map_element_value_update
   {
-    template<class x>
-    using f = typename detail::_unpack<tee<front<>, F, emp::wrapper<x>>, x>::type;
+    template<class kv>
+    using f = typename detail::_unpack<tee<front<>, F, emp::wrapper<kv>>, kv>::type;
   };
 
   /// Update an element `L<k, v...>` with `L<F<k, v...>, v...>`.
@@ -34,66 +34,66 @@ namespace jln::mp
   template<class F>
   struct map_element_key_update
   {
-    template<class x>
-    using f = typename detail::_unpack<partial_tee<F, emp::wrapper<x>>, x>::type;
+    template<class kv>
+    using f = typename detail::_unpack<partial_tee<F, emp::wrapper<kv>>, kv>::type;
   };
 
-  /// If the \map contain the key \c Key, replaces the existing element `L<k, v...>` with `L<F<k, v...>, v...>`.
+  /// If the \map contain the key \c key, replaces the existing element `L<k, v...>` with `L<F<k, v...>, v...>`.
   /// \pre \c is_map<>
   /// \treturn \sequence
-  template<class Key, class F, class C = listify>
-  using map_key_update = map_update<Key, map_element_key_update<F>, C>;
+  template<class key, class F, class C = listify>
+  using map_key_update = map_update<key, map_element_key_update<F>, C>;
 
-  /// If the \map contain the key \c Key, replaces the existing element `L<k, v...>` with `L<k, F<k, v...>>`.
+  /// If the \map contain the key \c key, replaces the existing element `L<k, v...>` with `L<k, F<k, v...>>`.
   /// \pre \c is_map<>
   /// \treturn \map
-  template<class Key, class F, class C = listify>
-  using map_value_update = map_update<Key, map_element_value_update<F>, C>;
+  template<class key, class F, class C = listify>
+  using map_value_update = map_update<key, map_element_value_update<F>, C>;
 
-  /// If the \map contain a key \c emp::front<KV>, replaces the existing element `L<k, v...>` with `F<k, v...>`; otherwise, inserts it using \c push_back<KV>.
+  /// If the \map contain a key \c emp::front<kv>, replaces the existing element `L<k, v...>` with `F<k, v...>`; otherwise, inserts it using \c push_back<kv>.
   /// \pre \c is_map<>
   /// \treturn \sequence
-  template<class KV, class F, class C = listify>
+  template<class kv, class F, class C = listify>
   using map_update_or_insert = if_<
-    map_contains<unpack<front<>>::f<KV>>,
-    map_update<unpack<front<>>::f<KV>, F, C>,
-    push_back<KV, C>
+    map_contains<unpack<front<>>::f<kv>>,
+    map_update<unpack<front<>>::f<kv>, F, C>,
+    push_back<kv, C>
   >;
 
-  /// If the \map contain a key \c emp::front<KV>, replaces the existing element `L<k, v...>` with `L<k, F<k, v...>>`; otherwise, inserts it using \c push_back<KV>.
+  /// If the \map contain a key \c emp::front<kv>, replaces the existing element `L<k, v...>` with `L<k, F<k, v...>>`; otherwise, inserts it using \c push_back<kv>.
   /// \pre \c is_map<>
   /// \treturn \map
-  template<class KV, class F, class C = listify>
-  using map_value_update_or_insert = map_update_or_insert<KV, map_element_value_update<F>, C>;
+  template<class kv, class F, class C = listify>
+  using map_value_update_or_insert = map_update_or_insert<kv, map_element_value_update<F>, C>;
 
 
   namespace emp
   {
-    template<class L, class Key, class F, class C = mp::listify>
-    using map_update = typename detail::_unpack<mp::map_update<Key, F, C>, L>::type;
+    template<class L, class key, class F, class C = mp::listify>
+    using map_update = typename detail::_unpack<mp::map_update<key, F, C>, L>::type;
 
-    template<class KV, class F>
+    template<class kv, class F>
     using map_element_key_update = typename mp::map_element_key_update<F>
-      ::template f<KV>;
+      ::template f<kv>;
 
-    template<class KV, class F>
+    template<class kv, class F>
     using map_element_value_update = typename mp::map_element_value_update<F>
-      ::template f<KV>;
+      ::template f<kv>;
 
-    template<class L, class Key, class F, class C = mp::listify>
+    template<class L, class key, class F, class C = mp::listify>
     using map_key_update =
-      typename detail::_unpack<mp::map_key_update<Key, F, C>, L>::type;
+      typename detail::_unpack<mp::map_key_update<key, F, C>, L>::type;
 
-    template<class L, class Key, class F, class C = mp::listify>
+    template<class L, class key, class F, class C = mp::listify>
     using map_value_update =
-      typename detail::_unpack<mp::map_value_update<Key, F, C>, L>::type;
+      typename detail::_unpack<mp::map_value_update<key, F, C>, L>::type;
 
-    template<class L, class KV, class F, class C = mp::listify>
+    template<class L, class kv, class F, class C = mp::listify>
     using map_update_or_insert =
-      typename detail::_unpack<mp::map_update_or_insert<KV, F, C>, L>::type;
+      typename detail::_unpack<mp::map_update_or_insert<kv, F, C>, L>::type;
 
-    template<class L, class KV, class F, class C = mp::listify>
+    template<class L, class kv, class F, class C = mp::listify>
     using map_value_update_or_insert =
-      typename detail::_unpack<mp::map_value_update_or_insert<KV, F, C>, L>::type;
+      typename detail::_unpack<mp::map_value_update_or_insert<kv, F, C>, L>::type;
   }
 }
--- include/jln/mp/smp/map/map_update.hpp
+++ include/jln/mp/smp/map/map_update.hpp
@@ -24,10 +24,10 @@ namespace jln::mp::detail
 
 namespace jln::mp::smp
 {
-  template<class Key, class F, class C = listify>
+  template<class key, class F, class C = listify>
   using map_update = test_contract<
     mp::is_map<>,
-    mp::map_update<Key, assume_unary<F>, subcontract<C>>
+    mp::map_update<key, assume_unary<F>, subcontract<C>>
   >;
 
   template<class F>
@@ -50,25 +50,25 @@ namespace jln::mp::smp
     >
   >;
 
-  template<class Key, class F, class C = listify>
+  template<class key, class F, class C = listify>
   using map_key_update = test_contract<
     mp::is_map<>,
-    mp::map_update<Key, detail::smp_map_element_key_update<assume_unary<F>>, subcontract<C>>
+    mp::map_update<key, detail::smp_map_element_key_update<assume_unary<F>>, subcontract<C>>
   >;
 
-  template<class Key, class F, class C = listify>
+  template<class key, class F, class C = listify>
   using map_value_update = test_contract<
     mp::is_map<>,
-    mp::map_update<Key, detail::smp_map_element_value_update<subcontract<F>>, subcontract<C>>
+    mp::map_update<key, detail::smp_map_element_value_update<subcontract<F>>, subcontract<C>>
   >;
 
-  template<class KV, class F, class C = listify>
-  using map_update_or_insert = typename detail::smp_map_update_or_insert<mp::is_map<>::f<KV>>
-    ::template f<KV, F, C>;
+  template<class kv, class F, class C = listify>
+  using map_update_or_insert = typename detail::smp_map_update_or_insert<mp::is_map<>::f<kv>>
+    ::template f<kv, F, C>;
 
-  template<class KV, class F, class C = listify>
-  using map_value_update_or_insert = typename detail::smp_map_update_or_insert<mp::is_map<>::f<KV>>
-    ::template f<KV, contract<detail::smp_map_element_value_update<subcontract<F>>>, C>;
+  template<class kv, class F, class C = listify>
+  using map_value_update_or_insert = typename detail::smp_map_update_or_insert<mp::is_map<>::f<kv>>
+    ::template f<kv, contract<detail::smp_map_element_value_update<subcontract<F>>>, C>;
 }
 
 
@@ -81,10 +81,10 @@ namespace jln::mp::detail
   template<>
   struct smp_map_update_or_insert<true_>
   {
-    template<class KV, class F, class C>
+    template<class kv, class F, class C>
     using f = test_contract<
       is_map<>,
-      map_update_or_insert<KV, assume_unary<F>, assume_unary_or_more<C>>
+      map_update_or_insert<kv, assume_unary<F>, assume_unary_or_more<C>>
     >;
   };
 
@@ -96,15 +96,15 @@ namespace jln::mp::detail
   template<class F>
   struct smp_map_element_key_update
   {
-    template<class x>
-    using f = typename detail::_unpack<partial_tee<F, monadic_xs<emp::wrapper<x>>>, x>::type;
+    template<class kv>
+    using f = typename detail::_unpack<partial_tee<F, monadic_xs<emp::wrapper<kv>>>, kv>::type;
   };
 
   template<class F>
   struct smp_map_element_value_update
   {
-    template<class x>
-    using f = typename detail::_unpack<tee<front<>, F, monadic_xs<emp::wrapper<x>>>, x>::type;
+    template<class kv>
+    using f = typename detail::_unpack<tee<front<>, F, monadic_xs<emp::wrapper<kv>>>, kv>::type;
   };
 
   template<template<class> class sfinae, class F>
@@ -120,51 +120,51 @@ namespace jln::mp::detail
   };
 
   // map_update
-  template<template<class> class sfinae, class Key, class F, class FC, class C>
-  struct _sfinae<sfinae, transform<map_find<Key, map_element_key_update<F>, FC>, C>>
+  template<template<class> class sfinae, class key, class F, class FC, class C>
+  struct _sfinae<sfinae, transform<map_find<key, map_element_key_update<F>, FC>, C>>
     : _sfinae<sfinae, transform<map_find<
-      Key,
+      key,
       contract<smp_map_element_key_update<assume_unary<sfinae<F>>>>, FC>,
       C
     >>
   {};
 
   // map_update
-  template<template<class> class sfinae, class Key, class F, class FC, class C>
-  struct _sfinae<sfinae, transform<map_find<Key, map_element_value_update<F>, FC>, C>>
+  template<template<class> class sfinae, class key, class F, class FC, class C>
+  struct _sfinae<sfinae, transform<map_find<key, map_element_value_update<F>, FC>, C>>
     : _sfinae<sfinae, transform<map_find<
-      Key,
+      key,
       contract<smp_map_element_value_update<subcontract<sfinae<F>>>>, FC>,
       C
     >>
   {};
 
   // map_update_or_insert
-  template<template<class> class sfinae, class Key, class KV, class F, class FC, class C1, class C2>
+  template<template<class> class sfinae, class key, class kv, class F, class FC, class C1, class C2>
   struct _sfinae<sfinae, if_<
-    map_contains<Key>,
-    transform<map_find<Key, map_element_key_update<F>, FC>, C1>,
-    push_back<KV, C2>
+    map_contains<key>,
+    transform<map_find<key, map_element_key_update<F>, FC>, C1>,
+    push_back<kv, C2>
   >>
-    : _sfinae<sfinae, if_<map_contains<Key>, transform<map_find<
-      Key,
+    : _sfinae<sfinae, if_<map_contains<key>, transform<map_find<
+      key,
       contract<smp_map_element_key_update<assume_unary<sfinae<F>>>>, FC>,
       C1
-    >, push_back<KV, C2>>>
+    >, push_back<kv, C2>>>
   {};
 
   // map_update_or_insert
-  template<template<class> class sfinae, class Key, class KV, class F, class FC, class C1, class C2>
+  template<template<class> class sfinae, class key, class kv, class F, class FC, class C1, class C2>
   struct _sfinae<sfinae, if_<
-    map_contains<Key>,
-    transform<map_find<Key, map_element_value_update<F>, FC>, C1>,
-    push_back<KV, C2>
+    map_contains<key>,
+    transform<map_find<key, map_element_value_update<F>, FC>, C1>,
+    push_back<kv, C2>
   >>
-    : _sfinae<sfinae, if_<map_contains<Key>, transform<map_find<
-      Key,
+    : _sfinae<sfinae, if_<map_contains<key>, transform<map_find<
+      key,
       contract<smp_map_element_value_update<subcontract<sfinae<F>>>>, FC>,
       C1
-    >, push_back<KV, C2>>>
+    >, push_back<kv, C2>>>
   {};
 }
 /// \endcond
