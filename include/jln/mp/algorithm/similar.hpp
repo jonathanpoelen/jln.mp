3378fd719414d7f0fa38ac0c99d2207b2d375fa6
Jonathan Poelen <jonathan.poelen@gmail.com>
2025-11-27 01:12:56 +0100

    fix similar with clang-20 and higher

--- include/jln/mp/algorithm/similar.hpp
+++ include/jln/mp/algorithm/similar.hpp
@@ -97,35 +97,31 @@ namespace jln::mp
 namespace jln::mp::detail
 {
   template<template<class...> class Tpl>
-  struct tpl_type1
+  struct tpl_tps
   {
-      using type = tpl_type1;
+      using type = tpl_tps;
   };
 
   template<template<class T, JLN_MP_TPL_AUTO_OR(T)...> class Tpl>
-  struct tpl_type2
+  struct tpl_tp_vals
   {
-      using type = tpl_type2;
-  };
-
-#if JLN_MP_ENABLE_TPL_AUTO
-  template<template<auto...> class Tpl>
-#else
-  template<template<class, std::size_t...> class Tpl>
-#endif
-  struct tpl_type3
-  {
-      using type = tpl_type3;
+      using type = tpl_tp_vals;
   };
 
   template<template<JLN_MP_TPL_AUTO_OR(std::size_t), class...> class Tpl>
-  struct tpl_type4
+  struct tpl_val_tps
   {
-      using type = tpl_type4;
+      using type = tpl_val_tps;
   };
 
 
 #if !JLN_MP_ENABLE_TPL_AUTO
+  template<template<class, std::size_t...> class Tpl>
+  struct tpl_tp_szs
+  {
+      using type = tpl_tp_szs;
+  };
+
   template<class T>
   struct normalize_similar3
   {
@@ -133,27 +129,58 @@ namespace jln::mp::detail
   };
 
   template<template<class, std::size_t...> class Tpl, class T, std::size_t... v>
-  struct normalize_similar3<Tpl<T, v...>> : tpl_type3<Tpl>
+  struct normalize_similar3<Tpl<T, v...>> : tpl_tp_szs<Tpl>
   {};
 
   template<class T>
   struct normalize_similar2 : normalize_similar3<T>
   {};
+
 #else // if JLN_MP_ENABLE_TPL_AUTO
+
+// fix not matching of `template<class T, T...>` with `Tpl<class, auto...>`
+# if JLN_MP_CLANG_LIKE >= 2000
+  template<template<class T, T...> class Tpl>
+  struct tpl_tp_tvals
+  {
+      using type = tpl_tp_tvals;
+  };
+
+  template<class T>
+  struct normalize_similar3
+  {
+    using type = T;
+  };
+
+  template<template<class T, T...> class Tpl, class T, T... v>
+  struct normalize_similar3<Tpl<T, v...>> : tpl_tp_tvals<Tpl>
+  {};
+
+  template<class T>
+  struct normalize_similar2 : normalize_similar3<T>
+  {};
+# else
   template<class T>
   struct normalize_similar2
   {
     using type = T;
   };
+# endif
+
+  template<template<auto...> class Tpl>
+  struct tpl_vals
+  {
+      using type = tpl_vals;
+  };
 
   template<template<auto...> class Tpl, auto... v>
-  struct normalize_similar2<Tpl<v...>> : tpl_type3<Tpl>
+  struct normalize_similar2<Tpl<v...>> : tpl_vals<Tpl>
   {};
 #endif
 
   template<template<class T, JLN_MP_TPL_AUTO_OR(T)...> class Tpl,
            class T, JLN_MP_TPL_AUTO_OR(T)... v>
-  struct normalize_similar2<Tpl<T, v...>> : tpl_type2<Tpl>
+  struct normalize_similar2<Tpl<T, v...>> : tpl_tp_vals<Tpl>
   {};
 
   template<class T>
@@ -161,12 +188,12 @@ namespace jln::mp::detail
   {};
 
   template<template<class...> class Tpl, class... T>
-  struct normalize_similar<Tpl<T...>> : tpl_type1<Tpl>
+  struct normalize_similar<Tpl<T...>> : tpl_tps<Tpl>
   {};
 
   template<template<JLN_MP_TPL_AUTO_OR(std::size_t), class...> class Tpl,
            JLN_MP_TPL_AUTO_OR(std::size_t) v, class... T>
-  struct normalize_similar<Tpl<v, T...>> : tpl_type4<Tpl>
+  struct normalize_similar<Tpl<v, T...>> : tpl_val_tps<Tpl>
   {};
 }
 /// \endcond
